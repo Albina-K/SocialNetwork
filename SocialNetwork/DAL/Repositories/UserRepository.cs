e3d7bd41bf7ed7e923f5a232ef2eabfe009492d9
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-07-23 14:09:08 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.DAL.Entities;
+
+namespace SocialNetwork.DAL.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public int Create(UserEntity userEntity)
+        {
+            return Execute(@"insert into users (firstname,lastname,password,email) 
+                             values (:firstname,:lastname,:password,:email)", userEntity);
+        }
+    }
+}
