8eba95dfbc3d3868b30f5f9776dbc630e7b9a4b7
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-08-02 17:00:34 +0300

    процесс реализации отправки сообщения

---
+++ SocialNetwork/Pll/Views/RegistrationView.cs
@@ -0,0 +1,35 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Pll.Views
+{
+    public class RegistrationView
+    {
+        UserService userService;
+        public RegistrationView(UserService userService)
+        {
+            this.userService = userService;
+        }
+        public void Show()
+        {
+            var userRegistrationData = new UserRegistrationData();
+
+            Console.WriteLine("Для создания нового профиля введите Ваше имя: ");
+            userRegistrationData.FirstName = Console.ReadLine();           
+            
+            Console.WriteLine("Ваша фамилия: ");
+            userRegistrationData.LastName = Console.ReadLine();
+            
+            Console.WriteLine("Пароль: ");
+            userRegistrationData.Password = Console.ReadLine();  
+            
+            Console.WriteLine("Почтовый адрес: ");
+            userRegistrationData.Email = Console.ReadLine();               
+        }
+    }
+}
