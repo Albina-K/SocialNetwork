42cf9f2b7bfba41cea718bb7dbcb55fbe3ecd930
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-08-08 11:52:05 +0300

    не проходит тест

--- SocilalNetwork.Tests/UserServiceTests.cs
+++ SocilalNetwork.Tests/UserServiceTests.cs
@@ -2,30 +2,22 @@ using SocialNetwork.BLL.Services;
 using SocialNetwork.BLL.Exceptions;
 using SocialNetwork.BLL.Models;
 using NUnit.Framework;
+using NUnit;
 
 namespace SocialNetwork.Tests
 {
     [TestFixture]
     public class UserServiceTests
-    {
-        private UserService userService;
-        private UserAuthenticationData userAuthenticationData;
-
-        public UserServiceTests()
-        {
-            userService = new UserService();
-            userAuthenticationData = new UserAuthenticationData();
-        }
+    {   
         [Test]
         public void Authenticate_MustThrowException()
         {
-            var userService = new UserService();
-            var userAuthenticationData = new UserAuthenticationData()
+            UserService userService = new UserService();
+            UserAuthenticationData userAuthenticationData = new UserAuthenticationData()
             {
                 Email = "gmail2@gmail.com",
                 Password = "fffffff"
-            };
-
+            };            
             Assert.Throws<WrongPasswordException>(() => userService.Authenticate(userAuthenticationData));
         }
     }
